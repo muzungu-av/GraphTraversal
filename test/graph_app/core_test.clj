4bde3c85001cd87d36c0c256bf30608bf93fd80a
Arykin Valera <muzungu.av@gmail.com>
2021-04-06 12:28:46 +0300

    tests

--- test/graph_app/core_test.clj
+++ test/graph_app/core_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [graph-app.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def G {:1 ['(:2 1) '(:3 2)]
+        :2 ['(:4 4)]
+        :3 ['(:4 2)]
+        :4 [] })
+
+(deftest traverse-test
+  (testing "Test traverse-graph-dfs function with weight graph."
+    (is (= (traverse-graph-dfs G :1) [:1 :3 :4 :2]))))
+
+
+(deftest dfs-test
+  (testing "Test seq-graph-dfs function with weight graph."
+    (is (= (seq-graph-dfs G :1)  [:1 :3 :4 :2]))))
+
+
+(deftest bfs-test
+  (testing "Test seq-graph-bfs function with weight graph."
+    (is (= (seq-graph-bfs G :1)  [:1 :2 :3 :4]))))
