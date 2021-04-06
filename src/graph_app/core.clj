70175fde82335a11c1d06b76864275acd7ad16bf
Arykin Valera <muzungu.av@gmail.com>
2021-04-06 12:07:22 +0300

    graph weight

--- src/graph_app/core.clj
+++ src/graph_app/core.clj
@@ -1,15 +1,15 @@
 (ns graph-app.core)
 
 (defn traverse-graph-dfs [g s]
-  (loop [vertices [] explored #{s} frontier [s]]   ;; вершины  исследованный граница
+  (loop [vertices [] explored #{s} frontier [s]]
     (if (empty? frontier)
       vertices
       (let [v (peek frontier)
-            neighbors (g v)]                                ;; соседи
+            neighbors (g v)]
         (recur
           (conj vertices v)
-          (into explored neighbors)
-          (into (pop frontier) (remove explored neighbors)))))))
+          (into explored (map #(nth % 0) neighbors) )
+          (into (pop frontier) (remove explored (map #(nth % 0) neighbors))))))))
 
 (defn seq-graph-dfs [g s]
   ((fn rec-dfs [explored frontier]
@@ -19,8 +19,8 @@
          (let [v (peek frontier)
                neighbors (g v)]
            (cons v (rec-dfs
-                     (into explored neighbors)
-                     (into (pop frontier) (remove explored neighbors))))))))
+                     (into explored (map #(nth % 0) neighbors))
+                     (into (pop frontier) (remove explored (map #(nth % 0) neighbors)))))))))
    #{s} [s]))
 
 (defn seq-graph-bfs [g s]
@@ -31,6 +31,6 @@
          (let [v (peek frontier)
                neighbors (g v)]
            (cons v (rec-bfs
-                     (into explored neighbors)
-                     (into (pop frontier) (remove explored neighbors))))))))
+                     (into explored (map #(nth % 0) neighbors))
+                     (into (pop frontier) (remove explored (map #(nth % 0) neighbors)))))))))
    #{s} (conj (clojure.lang.PersistentQueue/EMPTY) s)))
